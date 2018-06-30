178b92583e21fead40d785d499f28161672c2992
Aromal Sivadasan <aromal@zoko.io>
2018-06-30 15:42:22 +0530

    initial version

---
+++ zoko-services/src/main/java/io/zoko/rest/ZokoServicesApplication.java
@@ -0,0 +1,12 @@
+package io.zoko.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZokoServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZokoServicesApplication.class, args);
+	}
+}
